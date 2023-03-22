188d89fca496feb492830673fa6c317c4080426a
anex01 <mikhaelanene@gmail.com>
2023-03-22 05:13:08 -0700

    8 update

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,12 +1,16 @@
 #include "main.h"
+
 /**
  * jack_bauer - Prints the minutes of a day
  *
  * Return: no return
+ *
  */
+
 void jack_bauer(void)
 {
 	int a, b, c, d;
+
 	for (a = 0; a <= 2; a++)
 	{
 		for (b = 0; b <= 9; b++)
