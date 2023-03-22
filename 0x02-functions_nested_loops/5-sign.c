3d4d002457e3e3abd6205c18cb5d81e16ab99ed4
anex01 <mikhaelanene@gmail.com>
2023-03-22 04:16:19 -0700

    5 update

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,21 +1,30 @@
 #include "main.h"
 /**
- * _isalpha - Shows 1 if the input is a
- * letter Another cases, shows 0
+ * print_sign - Determines if the input number
+ * greater, equal or less that zero
  *
- * @c: The char in ASCII code
+ * @n: The input number as and interger.
  *
- * Return: 1 for letters. 0 for the ret.
+ * Return: 1 is greater than zero. 0 is zero
+ * -1 is less than zero
  */
-int _isalpha(int c)
+int print_sign(int n)
 {
-	if ((c >= 97 && c <= 122) || (c >= 65 && c <= 90))
+	if (n > 0)
 	{
+		_putchar (43);
 		return (1);
+	}
+	else if (n < 0)
+	{
+		_putchar (45);
+		return (-1);
 	}
 	else
 	{
+		_putchar (48);
 		return (0);
 	}
 	_putchar('\n');
 }
+
