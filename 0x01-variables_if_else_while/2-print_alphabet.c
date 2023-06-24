3d5603f1c4df294fca58a92db146f7515ae959b3
anex01 <mikhaelanene@gmail.com>
2023-06-24 16:58:26 -0700

    lower case update

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+/**
+ * main - print alphabets, lower case
+ * Return: (0)
+ */
+int main(void)
+{
+	int lower_case;
+
+	for (lower_case = 'a'; lower_case <= 'z'; lower_case++)
+		putchar(lower_case);
+		putchar('\n');
+
+	return (0);
+}
