ab41e78b00319e1552e807673cf998ed8ce1569d
anex01 <mikhaelanene@gmail.com>
2023-06-24 18:20:11 -0700

    0-9

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+/**
+ * main - print numbers 1 - 10
+ * Return: (0)
+ */
+int main(void)
+{
+	int single_digit;
+
+	for (single_digit = 0; single_digit <= 9; single_digit++)
+	{
+		printf("%d", single_digit);
+	}
+	return (0);
+
+}
