073fa74fe7db0459a3bd447f39276aefabec9de3
anex01 <mikhaelanene@gmail.com>
2023-06-26 15:54:06 -0700

    lowercase alphabets reverse

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - lowercase alphabets in reverse
+ * Return: (0)
+ */
+int main(void)
+{
+	int lower_case;
+
+	for (lower_case = 122; lower_case >= 97; lower_case--)
+
+	{
+		putchar(lower_case);
+
+	}
+		putchar('\n');
+
+	return (0);
+
+}
