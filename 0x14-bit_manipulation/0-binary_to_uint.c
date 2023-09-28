3f5dadd0c72c59856a5d84e6aa03982ff8f6355f
anex01 <mikhaelanene@gmail.com>
2023-09-28 11:44:47 -0700

    made changes to the binary to uint file

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,4 +1,5 @@
 #include "main.h"
+
 /**This converts a binary number to an unsigned integer
  * Return: the number that has been converted
  */
@@ -8,12 +9,12 @@ unsigned int binary_to_uint(const char *b)
 {
     unsigned int result = 0;
 
-    if (b == NULL)
+    if (!b)
         return 0;
 
-    for (int i = 0; b[i] != '\0'; i++)
+    for (int i = 0; b[i]; i++)
     {
-        if (b[i] != '0' && b[i] != '1')
+        if (b[i] < '0' || b[i] > '1')
             return 0;
 
         result = result * 2 + (b[i] - '0');
