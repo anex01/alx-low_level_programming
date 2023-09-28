bc5d8aff761add8b052a1433c795f336b7a24fa4
anex01 <mikhaelanene@gmail.com>
2023-09-28 11:30:09 -0700

    Added the _putchar.c file

---
+++ 0x14-bit_manipulation/_putchar.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <unistd.h>
+/**
+ * _putchar - writes character c to stdout
+ * @c: character to be displayed
+ *
+ * Return: 1 (Success)
+ * 	-1 (Error)
+ */
+int _putchar(char c)
+{
+	return (write(1, &c, 1));
+}
+
