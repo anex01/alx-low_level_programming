d1b390aed1c1f5ac3c5a4b194a764f8fbe510dbb
anex01 <mikhaelanene@gmail.com>
2023-09-29 09:36:39 -0700

    modification

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -13,7 +13,7 @@ unsigned int binary_to_uint(const char *b)
         return 0;
 
     int i;
-    for (i = 0; b[i]; i++)
+    for (int i = 0; b[i]; i++)
     {
         if (b[i] < '0' || b[i] > '1')
             return 0;
