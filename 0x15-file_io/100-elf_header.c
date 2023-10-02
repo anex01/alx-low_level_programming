a7318ab967e92183253a5d51b7d36a73435f806a
anex01 <mikhaelanene@gmail.com>
2023-10-02 13:27:36 -0700

    correction to code

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -1,36 +1,75 @@
 #include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
- * display_error - Display an error message and exit with status code 98.
- * @message: The error message to display.
+ * create_buffer - Allocates 1024 bytes for a buffer.
+ * @file: The name of the file buffer is storing chars for.
+ *
+ * Return: A pointer to the newly-allocated buffer.
  */
-void display_error(const char *message)
+char *create_buffer(char *file)
 {
-	fprintf(stderr, "%s\n", message);
-	exit(98);
+	char *buffer;
+
+	buffer = malloc(sizeof(char) * 1024);
+
+	if (buffer == NULL)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", file);
+		exit(99);
+	}
+
+	return (buffer);
 }
 
 /**
- * main - Display information from the ELF header of an ELF file.
- * @argc: The number of arguments.
- * @argv: The array of arguments.
- * Return: 0 on success, 98 on failure.
+ * close_file - Closes file descriptors.
+ * @fd: The file descriptor to be closed.
+ */
+void close_file(int fd)
+{
+	int result;
+
+	result = close(fd);
+
+	if (result == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - Copies the contents of a file to another file.
+ * @argc: The number of arguments supplied to the program.
+ * @argv: An array of pointers to the arguments.
+ *
+ * Return: 0 on success.
+ *
+ * Description: If the argument count is incorrect - exit code 97.
+ * If file_from does not exist or cannot be read - exit code 98.
+ * If file_to cannot be created or written to - exit code 99.
+ * If file_to or file_from cannot be closed - exit code 100.
  */
 int main(int argc, char *argv[])
 {
 	int file_from, file_to, read_result, write_result;
 	char *buffer;
 
-	if (argc != 2)
+	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "Usage: elf_header elf_filename\n");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
 
-	buffer = create_buffer(argv[1]);
+	buffer = create_buffer(argv[2]);
 	file_from = open(argv[1], O_RDONLY);
 	read_result = read(file_from, buffer, 1024);
-	file_to = open(argv[1], O_CREAT | O_WRONLY | O_TRUNC, 0664);
+	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
 	do {
 		if (file_from == -1 || read_result == -1)
@@ -45,13 +84,13 @@ int main(int argc, char *argv[])
 		if (file_to == -1 || write_result == -1)
 		{
 			dprintf(STDERR_FILENO,
-				"Error: Can't write to %s\n", argv[1]);
+				"Error: Can't write to %s\n", argv[2]);
 			free(buffer);
 			exit(99);
 		}
 
 		read_result = read(file_from, buffer, 1024);
-		file_to = open(argv[1], O_WRONLY | O_APPEND);
+		file_to = open(argv[2], O_WRONLY | O_APPEND);
 
 	} while (read_result > 0);
 
